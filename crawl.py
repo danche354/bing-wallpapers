d0c5fde82af148f9968c1d9b4f5d748743bdbf1d
danche354 <pengzonghui@foxmail.com>
2018-12-12 19:10:14 +0800

    Create crawl.py

---
+++ crawl.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+
+PARSE_URL = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=30'
+
+BASE_URL = 'https://www.bing.com'
+
+PIC_INFO = requests.get(PARSE_URL).json()['images'][0]
+
+DATE = PIC_INFO['startdate']
+PIC_PATH = PIC_INFO['url']
+
+PIC_URL = BASE_URL + PIC_PATH
+
+response = requests.get(PIC_URL)
+
+with open("/Users/pengzonghui/Pictures/bing-wallpapers/%s.jpg"%DATE, 'wb') as f:
+     f.write(response.content)
